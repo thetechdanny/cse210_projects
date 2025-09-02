de23d2e20ef4a638a71178a9b6b6f09d04ed0fac
thetechdanny <dannysamuelyoung@gmail.com>
2025-09-02 09:11:20 -0700

    completed the exercise 3 project with stretch challege

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string response = "yes";
+
+        while (response != "no")
+        {
+            if (response == "yes")
+            {
+                Random randomGenerator = new Random();
+                int number = randomGenerator.Next(1, 101);
+
+                int userGuess = 0;
+                int guessCount = 0;
+
+                while (userGuess != number)
+                {
+                    Console.Write("What is your guess? ");
+                    userGuess = Convert.ToInt32(Console.ReadLine());
+
+                    if (userGuess > number)
+                    {
+                        Console.WriteLine("Wrong! Guess lower");
+                    }
+
+                    else if (userGuess < number)
+                    {
+                        Console.WriteLine("Wrong! Guess higher");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Correct! You guessed it right!!!");
+                    }
+
+                    guessCount += 1;
+                }
+
+                Console.WriteLine($"You made {guessCount} guesses");
+                Console.Write("Do you want to play again? ");
+                response = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please enter 'yes' or 'no'");
+                response = Console.ReadLine();
+
+            }
+        }
+        Console.WriteLine("Thanks for playing!");
+
     }
 }
