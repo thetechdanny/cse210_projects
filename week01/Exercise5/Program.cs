feffcd77eb2cf187225861fd751bd7597b642ebb
thetechdanny <dannysamuelyoung@gmail.com>
2025-09-04 05:56:58 -0700

    completed the week five exericse

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Configuration.Assemblies;
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your username: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favourite number: ");
+        int userNumber = Convert.ToInt32(Console.ReadLine());
+        return userNumber;
+    }
+    static int SquareNumber(int number)
+    {
+        int squareNumber = number * number;
+        return squareNumber;
+    }
+
+    static string DisplayResult(string userName, int squareNumber)
+    {
+        string result = ($"{userName}, the square of your number is {squareNumber}");
+        return result;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        string result = DisplayResult(userName, squaredNumber);
+        Console.WriteLine(result);
     }
 }
