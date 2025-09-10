721727c6b173713ae182376fcb7169045b59d476
thetechdanny <dannysamuelyoung@gmail.com>
2025-09-10 08:29:01 -0700

    Resume Exercise completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Microsoft Engineer";
+        job1._startYear = 1990;
+        job1._endYear = 2000;
+        // job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Back-End Developer";
+        job2._startYear = 2000;
+        job2._endYear = 2005;
+        // job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume._name = "Daniel Kalu";
+
+        myResume.DisplayResume();
+
     }
 }
