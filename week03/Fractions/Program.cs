aa77bdba33e1e8e28a88ceed8c7b6d3cdde75338
thetechdanny <dannysamuelyoung@gmail.com>
2025-09-16 05:22:25 -0700

    Fraction exercise completed

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine("......................................");
+        Fraction f2 = new Fraction(6);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine("......................................");
+        Fraction f3 = new Fraction(6, 7);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine("......................................");
+        f3.SetTop(3);
+        f3.SetBottom(4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
     }
 }
